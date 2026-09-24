89cd93d875039a0a1e433491c9d564119358303a
Ishwari Moroney <ishwarimoroney595@gmail.com>
2026-09-24 14:27:22 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        String res = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = res.length() - 1;
+
+        while(left < right){
+            if(res.charAt(left) == res.charAt(right)){
+                left++;
+                right--;
+            }
+
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
