e20e938997e85792b04d302db3422e31a61f87b3
Ishwari Moroney <ishwarimoroney595@gmail.com>
2026-09-25 22:32:52 +0530

    Update submission-0.java

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int index1 = 0;
-        int index2 = numbers.length - 1;
+        int index1 = 0; // first position
+        int index2 = numbers.length - 1; // last position (avoids array index out of bound error)
 
 
         while(index1 < index2){
             int sum = numbers[index1] + numbers[index2];
 
             if(sum == target){
-                return new int[] { index1 + 1, index2 + 1};
+                return new int[] { index1 + 1, index2 + 1}; // 1-indexed indices
             }
 
             else if(sum < target){
